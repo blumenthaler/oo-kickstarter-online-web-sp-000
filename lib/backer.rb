09d0b6c3bad897895db4cfc4bfca062eac2a8b5a
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 18:15:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
-#  attr_accessor :backed_project
-  attr_reader :name, :backed_projects
+  attr_accessor :backed_projects
+  attr_reader :name
   
   def initialize(name)
     @name = name
